22a5393e78c2bfdc42b80dbbbb0937f32b200ee2
goofyseeker311 <goofyseeker311@gmail.com>
2024-11-28 10:52:47 +0200

    Initial commit.
    Implemented initial code in JavaVulkanCLRenderEngine.

---
+++ src/fi/jkauppa/javavulkanclrenderengine/JavaVulkanCLRenderEngine.java
@@ -0,0 +1,18 @@
+package fi.jkauppa.javavulkanclrenderengine;
+
+public class JavaVulkanCLRenderEngine {
+	public JavaVulkanCLRenderEngine() {
+	}
+	
+	public void run() {
+		
+	}
+
+	public static void main(String[] args) {
+		System.out.println("init.");
+		JavaVulkanCLRenderEngine app = new JavaVulkanCLRenderEngine();
+		app.run();
+		System.out.println("exit.");
+	}
+
+}
